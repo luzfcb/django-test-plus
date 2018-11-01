62c673f52462c14132586f9e7cf8e434e3374ac4
Fabio C. Barrionuevo da Luz <fabio.luz@britecore.com>
2018-11-01 19:48:28 -0300

    fix database filename and ignore sqlite3 files

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test_project'),
+        'NAME': os.path.join(BASE_DIR, 'test_project.sqlite3'),
     }
 }
 
